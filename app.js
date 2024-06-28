3f815afd18856a50404d49d0a21a11d9c5783b90
Abdul-Rafayy <155812890+Abdul-Rafayy@users.noreply.github.com>
2024-06-28 21:31:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,313 @@
+
+var cars = {
+    Mercedes: {
+        C_Class: {
+            carName: "Mercedes Benz C-Class",
+            model: "2024",
+            engine: "7000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "47 L",
+            image: 'images/C-Class.webp'
+        },
+        GLC: {
+            carName: "Mercedes Benz GLC",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/GLC.webp'
+        },
+        A_Class: {
+            carName: "Mercedes Benz A-Class Limousine",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/A-Class.webp'
+        },
+        G_Class: {
+            carName: "Mercedes Benz G-Class",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/G-Class.webp'
+        },
+        GLS: {
+            carName: "Mercedes Benz Maybach GLS",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/GLS.webp'
+        },
+        MaybachS_Class: {
+            carName: "Mercedes Benz Maybach S-Class",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/S-Class.webp'
+        },
+        GLA: {
+            carName: "Mercedes Benz GLA",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/GLA.webp'
+        },
+        E_Class: {
+            carName: "Mercedes Benz E-Class",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/E-class.webp'
+        },
+        S_Class: {
+            carName: "Mercedes Benz S-Class",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/S-Classs.webp'
+        },
+        AMG: {
+            carName: "Mercedes Benz AMG E53 Cabriolet",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/AMG.webp'
+        }
+    },
+    Audi: {
+        Q3: {
+            carName: "Audi Q3 Sportback",
+            model: "2024",
+            engine: "7000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "47 L",
+            image: 'images/Q3.webp'
+        },
+        EtronGT: {
+            carName: "Audi E-tron GT",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/EtronGT.webp'
+        },
+        S5: {
+            carName: "Audi S5 Sportback",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/S5.webp'
+        },
+        RS5: {
+            carName: "Audi RS5",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/RS5.webp'
+        },
+        A8: {
+            carName: "Audi A8 L",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/A8.webp'
+        },
+        A6: {
+            carName: "Audi A6",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/A6.webp'
+        },
+        Q7: {
+            carName: "Audi Q7",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/Q7.webp'
+        },
+        Q8: {
+            carName: "Audi RS Q8",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/Q8.webp'
+        },
+        Q5: {
+            carName: "Audi Q5",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/Q5.webp'
+        },
+        Etron: {
+            carName: "Audi E-tron Sportback",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/Etron.webp'
+        }
+    },
+    BMW: {
+        M340i: {
+            carName: "BMW M340i",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/M340i.webp'
+        },
+        Series7: {
+            carName: "BMW 7 Series",
+            model: "2024",
+            engine: "7000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "47 L",
+            image: 'images/series7.webp'
+        },
+        Series2: {
+            carName: "BMW 2 Series",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/series2.webp'
+        },
+        Series3: {
+            carName: "BMW 3 Series",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/series3.webp'
+        },
+        M8: {
+            carName: "BMW M8",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/M8.webp'
+        },
+        M2: {
+            carName: "BMW M2",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/M2.webp'
+        },
+        M4: {
+            carName: "BMW M4",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/M4.webp'
+        },
+        Z4: {
+            carName: "BMW Z4",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/Z4.webp'
+        },
+        XM: {
+            carName: "BMW XM",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/XM.webp'
+        },
+        X7: {
+            carName: "BMW X7",
+            model: "2022",
+            engine: "6000",
+            seatCapacity: "5 seater",
+            fuelType: "Petrol",
+            fuelTankCapacity: "42 L",
+            image: 'images/X7.webp'
+        }
+
+    }
+};
+
+
+var company = document.getElementById("company")
+var model = document.getElementById("model")
+for (var key in cars) {
+    company.innerHTML += `<option value = '${key}'>${key}</option>`
+}
+
+function selectCompany() {
+    model.innerHTML = "<option>Select Model</option>"
+    for (var key in cars[company.value]) {
+        model.innerHTML += `<option value = '${key}'>${key}</option>`
+
+    }
+
+}
+
+
+function search() {
+    var out = document.getElementById("out");
+    out.innerHTML = "",
+        out.innerHTML = `  
+        <div class="main">
+            <img src="${cars[company.value][model.value].image}" alt="">
+            <h2>  ${cars[company.value][model.value].carName}</h2>
+            <p> Model: ${cars[company.value][model.value].model}</p>
+            <p> Seat capacity: ${cars[company.value][model.value].seatCapacity}</p>
+            <p> Tank Capacity: ${cars[company.value][model.value].fuelTankCapacity}</p>
+            <p> Fuel type: ${cars[company.value][model.value].fuelType}</p>
+            <p> Engine: ${cars[company.value][model.value].engine}</p>
+        </div>
+`;
+}
